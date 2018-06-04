9c4cb98bf7051c8e594718e1f9b2e9438665014d
wudi <122895950@qq.com>
2018-06-04 11:41:22 +0800

    update

--- monitor-server/src/main/java/cn/willu/cloud/monitorserver/MonitorServerApplication.java
+++ monitor-server/src/main/java/cn/willu/cloud/monitorserver/MonitorServerApplication.java
@@ -1,12 +1,28 @@
 package cn.willu.cloud.monitorserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableDiscoveryClient
+@EnableAdminServer
 @SpringBootApplication
 public class MonitorServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
     }
 }
