20c11d75cb7a2018bee6a3d6a69c94f2e28a29f2
wudi <122895950@qq.com>
2018-06-06 11:05:20 +0800

    formate

--- discovery-server/src/test/java/cn/willu/cloud/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/cn/willu/cloud/discoveryserver/DiscoveryServerApplicationTests.java
@@ -10,5 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DiscoveryServerApplicationTests {
 
     @Test
-    public void contextLoads() {}
+    public void contextLoads() {
+    }
 }
--- zuul-server/src/test/java/cn/willu/cloud/zuulserver/ZuulServerApplicationTests.java
+++ zuul-server/src/test/java/cn/willu/cloud/zuulserver/ZuulServerApplicationTests.java
@@ -10,5 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ZuulServerApplicationTests {
 
     @Test
-    public void contextLoads() {}
+    public void contextLoads() {
+    }
 }
