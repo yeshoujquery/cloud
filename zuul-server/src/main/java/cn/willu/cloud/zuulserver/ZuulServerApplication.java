534377d838cf737768574cdb8b2d7e456067f3dd
wudi <122895950@qq.com>
2018-06-06 11:20:25 +0800

    formate

--- zuul-server/src/main/java/cn/willu/cloud/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/cn/willu/cloud/zuulserver/ZuulServerApplication.java
@@ -2,7 +2,11 @@ package cn.willu.cloud.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
+@EnableZuulServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulServerApplication {
 
