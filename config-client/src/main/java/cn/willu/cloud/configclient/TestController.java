5c3ae3831322b9bdc5ab41bc23137bacd02e7bb4
wud <wud@postar.cn>
2018-06-24 16:34:40 +0800

    高可用 注册中心

---
+++ config-client/src/main/java/cn/willu/cloud/configclient/TestController.java
@@ -0,0 +1,21 @@
+package cn.willu.cloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestController {
+    @Value("${from}")
+    private String from;
+
+    @GetMapping("/from")
+    public String getFrom(){
+        return this.from;
+    }
+
+}
