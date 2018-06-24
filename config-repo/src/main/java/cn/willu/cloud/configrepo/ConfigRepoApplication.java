751035c3c044db106700433bee883b3b77714868
wud <wud@postar.cn>
2018-06-24 16:17:50 +0800

    高可用 注册中心

---
+++ config-repo/src/main/java/cn/willu/cloud/configrepo/ConfigRepoApplication.java
@@ -0,0 +1,12 @@
+package cn.willu.cloud.configrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigRepoApplication.class, args);
+    }
+}
